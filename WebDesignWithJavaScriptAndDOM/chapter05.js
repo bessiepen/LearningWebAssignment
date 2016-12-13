7d1ce92c68746db4c4753a11ca2904bba8865499
pengyao <penggeyao@gmail.com>
2016-12-13 21:32:40 +0800

    window.open()

---
+++ WebDesignWithJavaScriptAndDOM/chapter05.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  if (!document.getElementsByTagName) return false;
+  var lnks = document.getElementsByTagName("a");
+  for (var i = 0; i < lnks.length; i++) {
+    if (lnks[i].getAttribute("class") == "popup") {
+      lnks[i].onclick = function() {
+        popUp(this.getAttribute("href"));
+        return false;
+      }
+    }
+  }
+}
+
+function popUp(winURL) {
+  window.open(winURL,"popup","width=320,height=480");
+}
+
+// note:
+//window.open("URL","name","specs","replace")
+// name: _blank; _self; _parent; _top; name;
+// specs: width;height;
