7f23aa0169629aac8e6cfd9b56e27c773b240ce6
pengyao <penggeyao@gmail.com>
2016-12-13 21:46:13 +0800

    this & addLoadEvent(func) & onclick

---
+++ WebDesignWithJavaScriptAndDOM/chapter06ImageGallery.js
@@ -0,0 +1,143 @@
+// chapter06 - 01
+
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder")) return false;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  placeholder.setAttribute("src",source);
+  if (!document.getElementById("description")) return false;
+  if (whichpic.getAttribute("title")) {
+    var text = whichpic.getAttribute("title");
+  } else {
+    var text = "";
+  }
+  var description = document.getElementById("description");
+  if (description.firstChild.nodeType == 3) {
+    description.firstChild.nodeValue = text;
+  }
+  return false;
+}
+
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = function() {
+      return showPic(this);
+    }
+    links[i].onkeypress = links[i].onclick;
+  }
+}
+// addLoadEvent
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload =function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(prepareGallery);
+
+// chapter06 - 02
+
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder")) return false;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  placeholder.setAttribute("src",source);
+  if (!document.getElementById("description")) return false;
+  if (whichpic.getAttribute("title")) {
+    var text = whichpic.getAttribute("title");
+  } else {
+    var text = "";
+  }
+  var description = document.getElementById("description");
+  if (description.firstChild.nodeType == 3) {
+    description.firstChild.nodeValue = text;
+  }
+  return false;
+}
+
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for ( var i=0; i<links.length; i++) {
+    links[i].onclick = function() {
+      return showPic(this); // this 指的 link[i]
+    }
+    links[i].onkeypress = links[i].onclick;
+  }
+}
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(prepareGallery);
+
+// chapter06 - 03
+
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder")) return false;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  placeholder.setAttribute("src",source);
+  if (!document.getElementById("description")) return false;
+  if (whichpic.getAttribute("title")) {
+    var text = whichpic.getAttribute("title");
+  } else {
+    var text = "";
+  }
+  var description = document.getElementById("description");
+  if (description.firstChild.nodeType == 3) {
+    description.firstChild.nodeValue = text;
+  }
+  return false;
+}
+
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var gallery = documetn.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for (var i=0; i<links.length; i++) {
+    links[i].onclick = function() {
+      return showPic(this);
+    }
+    links[i].onkeypress = links[i].onclick;
+  }
+}
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(prepareGallery);
