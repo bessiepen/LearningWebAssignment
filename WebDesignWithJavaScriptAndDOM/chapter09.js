661ba8b0fb191bbd72c1da480a1653c877d64328
pengyao <penggeyao@gmail.com>
2016-12-14 12:39:14 +0800

    style.fontFamily

---
+++ WebDesignWithJavaScriptAndDOM/chapter09.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  var para = document.getElementById("example");
+  para.style.color = "black";
+  para.style.font = "2em 'Times',serif";
+  alert("The font family is " + para.style.fontFamily); // font-family = fontFamily
+}
