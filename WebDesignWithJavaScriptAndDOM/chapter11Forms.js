6807c69251dea03000eb5fb335709a098df6aa28
pengyao <penggeyao@gmail.com>
2016-12-14 22:55:51 +0800

    input.onfocus & input.onblur

---
+++ WebDesignWithJavaScriptAndDOM/chapter11Forms.js
@@ -0,0 +1,17 @@
+// chapter11 - forms
+
+if (!Modernizr.input.placeholder) {
+  var input = document.getElementById('email');
+  input.onfocus = function() {
+    var text = this.placeholder || this.getAttribute('placeholder');
+    if ( this.value == text ) {
+      this.value = '';
+    }
+    input.onblur = function() {
+      if (this.value == '') {
+        this.value = this.placeholder || this.getAttribute('placeholder');
+      }
+    }
+    input.onblur(); //The onblur event occurs when an object loses focus.
+  }
+}
