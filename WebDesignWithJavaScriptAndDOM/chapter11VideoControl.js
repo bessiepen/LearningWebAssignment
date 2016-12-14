4f208941fb5bb99896e14ffd173eb29a6c8ad92d
pengyao <penggeyao@gmail.com>
2016-12-14 22:54:32 +0800

    removeAttribute() & addEventListener()

---
+++ WebDesignWithJavaScriptAndDOM/chapter11VideoControl.js
@@ -0,0 +1,53 @@
+// chapter11 - video custom controls
+
+(function() {
+
+function createVideoControls() {
+  var vids = document.getElementsByTagName('video');
+  for (var i = 0; i < vids.length; i++) {
+    addControls( vid[i] );
+  }
+}
+function addControls( vids ) {
+  vid.removeAttribute('controls');
+  vid.height = vid.videoHeight;
+  vid.width = vid.videoWidth;
+  vid.parentNode.style.height = vid.videoHeight + 'px';
+  vid.parentNode.style.width = vid.videoWidth + 'px';
+  var controls = document.createElement('div');
+  controls.setAttribute('class','controls');
+  var play = document.createElement('botton');
+  play.setAttribute('title','Play');
+  play.innerHTML = '&#x25BA;';
+  controls.appendChild(play);
+  vid.parentNode.insertBefore(controls, vid);
+  play.onclick = function() {
+    if (vid.ended) {
+      vid.currentTime = 0;
+    }
+    if (vid.paused) {
+      vid.play();
+    } else {
+      vid.pause();
+    }
+  };
+  vid.addEventListener('play', function() {
+    play.innerHTML = '&#x2590;&#x2590;';
+    play.setAttribute('paused',true);
+  }, false);
+  vid.addEventListener('pause', function() {
+    play.removeAttribute('paused');
+    play.innerHTML = '&#25BA;';
+  }, false);
+  vid.addEventListener('ended', function() {
+    vid.pause();
+  }, false);
+}
+// note: addEventListener(event,function,useCapture)
+// useCapture: true (The event handler is executed in the capturing phase) / false ( Default. The event handler is executed in the bubbling phase)
+// what is capturing/bubbling phase: https://javascript.info/tutorial/bubbling-and-capturing
+window.onload = function() {
+  createVideoControls();
+}
+
+})()
