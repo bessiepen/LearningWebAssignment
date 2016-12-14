bf9dcfbc14684e3a0032577aebbb12032777d72b
pengyao <penggeyao@gmail.com>
2016-12-14 12:48:06 +0800

    onmouseover & onmouseout & className

--- WebDesignWithJavaScriptAndDOM/chapter09itineray.js
+++ WebDesignWithJavaScriptAndDOM/chapter09itineray.js
@@ -86,3 +86,90 @@ function addClass(element,value) {
   }
 }
 addLoadEvent(stripeTables);
+
+// chapter09 - itineray repeat1
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+function displayAccesskeys() {
+  if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;
+  var abbreviations = document.getElementsByTagName("abbr");
+  if (abbreviations.length < 1) return false;
+  var defs = new Array();
+  for (var i=0; i<abbreviations.length; i++) {
+    var current_abbr = abbreviations[i];
+    if (current_abbr.childNodes.length < 1) continue;
+    var definition = current_abbr.getAttribute("title");
+    var key = current_abbr.lastChild.nodeValue;
+    defs[key] = definition;
+  }
+  var dlist = document.createElement("dl");
+  for (key in defs) {
+    var definition = defs[key];
+    var dtitle = document.createElement("dt");
+    var dtitle_text = document.createTextNode(key);
+    dtitle.appendChild(dtitle_text);
+    var ddesc = document.createElement("dd");
+    var ddesc_text = document.createTextNode(definition);
+    ddesc.appendChild(ddesc_text);
+    dlist.appendChild(dtitle);
+    dlist.appendChild(ddesc);
+  }
+  if (dlist.childNodes.length < 1) return false;
+  var header = document.createElement("h2");
+  var header_text = document.createTextNode("Abbreviations");
+  header.appendChild(header_text);
+  document.body.appendChild(header);
+  document.body.appendChild(dlist);
+}
+addLoadEvent(displayAbbreviations);
+
+function highlightRows() {
+  if (!document.getElementsByTagName) return false;
+  var rows = document.getElementsByTagName("tr");
+  for (var i=0; i<rows.length; i++) {
+    rows[i].onmouseover = function() {
+      this.style.fontWeight = "bold";
+    }
+    rows[i].onmouseout = function() {
+      this.style.fontWeight = "normal";
+    }
+  }
+}
+addLoadEvent(highlightRows);
+function stripeTables() {
+  if (!document.getElementsByTagName) return false;
+  var tables = document.getElementsByTagName("table");
+  for (var i=0; i<tables.length; i++) {
+    var odd = false;
+    var rows = tables[i].getElementsByTagName("tr");
+    for (var j=0; j<rows.length; j++) {
+      if (odd == true) {
+        addClass(rows[i],"odd");
+        odd = false;
+      } else {
+        odd = true;
+      }
+    }
+  }
+}
+function addClass(elemnt,value) {
+  if (!element.className) {
+    element.className = value;
+  } else {
+    newClassName = element.className;
+    newClassName += " ";
+    newClassName += value;
+    element.className = newClassName;
+  }
+}
+addLoadEvent(stripeTables);
