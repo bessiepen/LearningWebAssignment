94b8e740c655f634c37420e450009ac9b2253762
pengyao <penggeyao@gmail.com>
2016-12-23 16:39:53 +0800

    Logical && Operator

---
+++ ReactQuickStartCoding/conditional-rendering-inline-if.js
@@ -0,0 +1,49 @@
+// eg1: Inline If with Logical && Operator
+// `true && expression`继续跑expression
+// `false && expression`为false，停止跑expression
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  );
+}
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+ReactDOM.render(
+  <Mailbox unreadMessages={messages} />,
+  document.getElementById('root')
+);
+
+
+
+// eg2: condition ? true : false
+
+render() {
+  const isLoggedIn = this.state.isLoggedIn;
+  return (
+    <div>
+      The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in.
+    </div>
+  );
+}
+
+// 或者
+
+render() {
+  const isLoggedIn = this.state.isLoggedIn;
+  return (
+    <div>
+      {isLoggedIn ? (
+        <LogoutButton onClick={this.handleLogoutClick} />
+      ) : (
+        <LoginButton onClick={this.handleLoginClick} />
+      )}
+    </div>
+  );
+}
