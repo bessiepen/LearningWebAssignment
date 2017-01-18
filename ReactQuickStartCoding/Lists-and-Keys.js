96ffcc1ecb84ac75e64cdf9ac2883dbf9df93ab6
pengyao <penggeyao@gmail.com>
2017-01-18 20:07:12 +0800

    Create Lists-and-Keys.js

---
+++ ReactQuickStartCoding/Lists-and-Keys.js
@@ -0,0 +1,91 @@
+// use map() to change array into list
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((number) => number * 2);
+console.log(doubled);
+
+
+// map()
+
+const numbers = [1, 2, 3, 4, 5];
+const listIetms = numbers.map((number) =>
+                           <li>{number}</li>  
+                            );
+ReactDOM.render(
+  <ul>{listIetms}</ul>,
+  document.getElementById('root')  
+); 
+
+
+// 
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li>{number}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5]
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
+);
+
+
+// Keys
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+const numbers = [1, 2, 3, 4, 5]
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
+);
+
+
+// Keys 与sibling要独一无二，但是全局上可以重复使用
+
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+}                                   
+const posts = [
+  {id:1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id:2, title: 'Installation', content: 'You can install React from npm.'}
+];
+ReactDOM.render(
+  <Blog posts={posts} />,
+  document.getElementById('root')
+);                              
