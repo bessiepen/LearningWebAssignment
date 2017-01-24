1a485a7ec33395d6ba9f1fd3bc01fab06ca44132
bessiepen <penggeyao@gmail.com>
2017-01-24 15:42:41 +0800

    copy example
    根据原例子抄写

--- ReactQuickStartCoding/thinking-in-react.js
+++ ReactQuickStartCoding/thinking-in-react.js
@@ -1,40 +1,69 @@
 class ProductCategoryRow extends React.Component {
-  return() {
-    <ProductTable>
-      <p>{props.category}</p>
-      {props.children}
-    </ProductTable>
+  render() {
+    return <tr><th>{this.props.category}</th></tr>;
   }
 }
 class ProductRow extends React.Component {
-  return() {
-    <ProductCategoryRow>
-      <p>{props.name}+{props.price}</p>
-    </ProductCategoryRow>
+  render() {
+    var name = this.props.product.stocked ? this.props.product.name :
+    <span>
+      {this.props.product.name}
+    </span>;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    );
   }
 }
 class ProductTable extends React.Component {
-  return() {
-    <div>
-      <p>Name   Price</p>
-      {props.children}
-    </div>
+  render() {
+    var rows = [];
+    var lastCategory = null;
+    // The forEach() method executes a provided function once for each array element.
+    this.props.products.forEach(function(product) {
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
   }
 }
 class SearchBar extends React.Component {
-  return() {
-    <div>
-      <input value={value} />
-      <input type="radio" />
-    </div>
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
   }
 }
 class FilterableProductTable extends React.Component {
-  return() {
-    <div>
-      <SearchBar />
-      <ProductTable />
-    </div>
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products}/>
+      </div>
+    );
   }
 }
 var PRODUCTS = [
@@ -45,3 +74,7 @@ var PRODUCTS = [
   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
 ];
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById('container')
+);
